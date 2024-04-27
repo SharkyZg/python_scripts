9e3264a65de1fa9bffd1ee78e4f7cc031610b2e6
Marko Sarkanj <markosarkanj@Markos-MBP-M1.fritz.box>
2024-04-27 12:42:27 +0200

    z_buffer wip 2

--- z_buffer.py
+++ z_buffer.py
@@ -1,7 +1,5 @@
 import numpy as np
-import pygame
 from geometry_shapes import *
-import statistics
 
 class ZBuffer:
     def __init__(self, width: int, height: int):
@@ -20,23 +18,3 @@ class ZBuffer:
 
     def get_z_value(self, x: int, y: int) -> float:
         return self.buffer[y, x]
-
-def render_polygon(screen, polygons, z_buffer, color):
-    for polygon in polygons:
-        vertices_2d = polygon[:][:2]
-        min_x, max_x, min_y, max_y = bounding_box(vertices_2d)
-        print(f"min_x: {min_x}, max_x: {max_x}, min_y: {min_y}, max_y: {max_y}")
-        z_avg = np.array(polygon[:][2]).mean()
-        for y in range(int(min_y), int(max_y) + 1):
-            for x in range(int(min_x), int(max_x) + 1):
-                if z_buffer.test_and_set(x, y, z_avg):
-                    pygame.draw.rect(screen, color, pygame.Rect(x, y, 1, 1))
-                    print(f"x: {x}, y: {y}, z_avg: {z_avg}, color: {color}")
-
-def bounding_box(vertices):
-    vertices = np.array(vertices)
-    min_x = vertices[0].min()
-    max_x = vertices[0].max()
-    min_y = vertices[1].min()
-    max_y = vertices[1].max()
-    return min_x, max_x, min_y, max_y
