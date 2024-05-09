fd52b1a45acb1c647ae0bb31b63e7f58cc7a91f1
Marko Sarkanj <marko.sarkanj@gmail.com>
2024-05-09 10:45:17 +0200

    shapes changing started

---
+++ shapes_changing_wip.py
@@ -0,0 +1,64 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import math
+
+def CubeToSphere(t):
+    vertices = []
+    num_slices = 16
+    num_stacks = 8
+
+    for i in range(num_stacks + 1):
+        z = (2 * i / num_stacks - 1)
+        r = math.sqrt(1 - z ** 2)
+
+        for j in range(num_slices):
+            theta = 2 * math.pi * j / num_slices
+            x = r * math.cos(theta) * (1 - t) + (t * math.cos(theta))
+            y = r * math.sin(theta) * (1 - t) + (t * math.sin(theta))
+
+            vertices.append([x, y, z])
+
+    glBegin(GL_QUAD_STRIP)
+
+    for i in range(num_stacks):
+        for j in range(num_slices):
+            glVertex3fv(vertices[i * num_slices + j])
+            glVertex3fv(vertices[(i + 1) * num_slices + j])
+
+    glEnd()
+
+def main():
+    pygame.init()
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+
+    glTranslatef(0.0,0.0, -5) 
+
+    glEnable(GL_DEPTH_TEST)  
+
+    t = 0
+    shape_change_rate = 0.003
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(1, 3, 6, 1)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        CubeToSphere(t)
+
+        t += shape_change_rate
+        if t > 2:
+            shape_change_rate *= -1
+        elif t < -1:
+            shape_change_rate *= -1
+
+        pygame.display.flip() 
+        pygame.time.wait(10)
+
+main()
